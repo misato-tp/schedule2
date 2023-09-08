f284e446fff29ef92b4cc4fb70e3fcecd15d7017
user <user@usernoMacBook-Air.local>
2023-09-09 01:01:21 +0900

    自作バリデーションについて

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,10 +2,14 @@ class Event < ApplicationRecord
     validates :title, length:{maximum:20} 
     validates :memo, length:{maximum:500} 
     validates :title, presence: true 
-    validates :start_date, :end_date, presence: true, if: :start_end_check
+    validates :start_date, :end_date, presence: true
+    validate :start_end_check
 
     def start_end_check
-        errors.add(:end, "は開始日以降の日付で選択して下さい。") unless
-        self.start_date < self.end_date
+        binding.pry
+        if start_date.nil? && end_date.nil? == false
+            start_date > end_date
+            errors.add(:end, "は開始日以降の日付で選択して下さい。")
+        end
     end
 end
