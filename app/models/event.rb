2aa8a982fff007c46143e6160f4b29e56bad3a36
user <user@usernoMacBook-Air.local>
2023-09-08 15:33:43 +0900

    バリデーション質問

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,11 @@
 class Event < ApplicationRecord
-    validates :title, length:{maximum:20} #タイトルは20文字まで
-    validates :memo, length:{maximum:500} #スケジュールメモは最大で500文字
-    validates :title,:start_date,:end_date, presence: true #必須項目が入力できているか
-    validate :start_end_check
+    validates :title, length:{maximum:20} 
+    validates :memo, length:{maximum:500} 
+    validates :title, presence: true 
+    validates :start_date, :end_date, presence: true, if: :start_end_check
 
     def start_end_check
-        if start_date.after? end_date
-        errors.add(:end, "は開始日以降の日付で選択して下さい。")
-        end
+        errors.add(:end, "は開始日以降の日付で選択して下さい。") unless
+        self.start_date < self.end_date
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_06_042754) do
+ActiveRecord::Schema.define(version: 2023_09_07_150713) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
