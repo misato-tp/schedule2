59eb6846a7309577d4c89a24b68af56196dc5239
user <user@usernoMacBook-Air.local>
2023-09-11 00:58:41 +0900

    修正後1

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,16 @@
 class Event < ApplicationRecord
     validates :title, length:{maximum:20} 
     validates :memo, length:{maximum:500} 
-    validates :title, :start_date, :end_date, presence: true, if: :start_end_check
+    validates :title, presence: true
+    validates :start_date, presence:true, if: :start_date.nil?
+    validates :end_date, presence:true, if: :end_date.nil?
+    validate :start_end_check
 
-    def start_end_check
-        if self.start_date > self.end_date
-        errors.add(:end,"は開始日以降の日付で選択してください。")
+    def start_end_check 
+        return unless start_date && end_date #開始日と終了日の日付が入ってなかったらretuenされる
+
+        if !end_date.after?(start_date) #引数の開始日が終了日より後になってない（！）
+            errors.add(:end,"は開始日以降の日付で選択してください。")
         end
     end
 end
